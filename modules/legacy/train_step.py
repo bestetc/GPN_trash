95d57427f04489aa2667ba62361044f6d5a0a9d3
Vadim <sobolevskiy.vadim@gmail.com>
2021-05-17 09:00:15 +0000

    delete legacy modules

--- modules/legacy/train_step.py
+++
@@ -1,9 +0,0 @@
-def make_step(data, optimizer, model, criterion, device):
-    ''' Train loop in one function. '''
-    inputs, labels = data[0].to(device), data[1].to(device)
-    optimizer.zero_grad()
-    outputs = model(inputs)
-    loss = criterion(outputs, labels)
-    loss.backward()
-    optimizer.step()
-    return loss, outputs
